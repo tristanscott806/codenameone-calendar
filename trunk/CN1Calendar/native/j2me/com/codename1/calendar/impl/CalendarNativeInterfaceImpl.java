4c1c4da4c8faeae4d9cec36fa66e0b459ab26831
shai.almog@gmail.com <shai.almog@gmail.com@2ff2903f-4249-a15f-21a5-a09108b5d3d4>
2014-01-29 16:30:09 +0000

    Initial stubs for the project

---
+++ trunk/CN1Calendar/native/j2me/com/codename1/calendar/impl/CalendarNativeInterfaceImpl.java
@@ -0,0 +1,34 @@
+package com.codename1.calendar.impl;
+
+public class CalendarNativeInterfaceImpl {
+    public String getEvents(long param, long param1) {
+        return null;
+    }
+
+    public String saveEvent(String param, String param1, long param2, long param3, boolean param4, boolean param5, String param6, String param7, String param8) {
+        return null;
+    }
+
+    public void deregisterForEventNotifications() {
+    }
+
+    public void registerForEventNotifications() {
+    }
+
+    public boolean hasPermissions() {
+        return false;
+    }
+
+    public String getEventByIdentifier(String param) {
+        return null;
+    }
+
+    public boolean removeEvent(String param) {
+        return false;
+    }
+
+    public boolean isSupported() {
+        return false;
+    }
+
+}
---
+++ trunk/CN1Calendar/native/win/com/codename1/calendar/impl/CalendarNativeInterfaceImpl.cs
@@ -0,0 +1,35 @@
+namespace com.codename1.calendar.impl{
+
+public class CalendarNativeInterfaceImpl {
+    public String  getEvents(long param, long param1) {
+        return null;
+    }
+
+    public String  saveEvent(String param, String param1, long param2, long param3, boolean param4, boolean param5, String param6, String param7, String param8) {
+        return null;
+    }
+
+    public void deregisterForEventNotifications() {
+    }
+
+    public void registerForEventNotifications() {
+    }
+
+    public bool hasPermissions() {
+        return false;
+    }
+
+    public String  getEventByIdentifier(String param) {
+        return null;
+    }
+
+    public bool removeEvent(String param) {
+        return false;
+    }
+
+    public bool isSupported() {
+        return false;
+    }
+
+}
+}
