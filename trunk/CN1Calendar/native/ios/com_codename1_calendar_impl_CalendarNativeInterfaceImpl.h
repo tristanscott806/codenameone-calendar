e08a3bd01cb539eaf90c03e2df086e5fa9212c33
lanerolle@gmail.com <lanerolle@gmail.com@2ff2903f-4249-a15f-21a5-a09108b5d3d4>
2014-02-03 04:51:08 +0000

    iOS implementation of the native interface

--- trunk/CN1Calendar/native/ios/com_codename1_calendar_impl_CalendarNativeInterfaceImpl.h
+++ trunk/CN1Calendar/native/ios/com_codename1_calendar_impl_CalendarNativeInterfaceImpl.h
@@ -1,14 +1,18 @@
 #import <Foundation/Foundation.h>
+#import <EventKit/EventKit.h>
 
 @interface com_codename1_calendar_impl_CalendarNativeInterfaceImpl : NSObject {
 }
 
--(NSString*)getEvents:(long long)param param1:(long long)param1;
--(NSString*)saveEvent:(NSString*)param param1:(NSString*)param1 param2:(long long)param2 param3:(long long)param3 param4:(BOOL)param4 param5:(BOOL)param5 param6:(NSString*)param6 param7:(NSString*)param7 param8:(NSString*)param8;
--(void)deregisterForEventNotifications;
--(void)registerForEventNotifications;
+-(NSString*)getCalendarName:(int)param;
+-(int)getCalendarCount;
 -(BOOL)hasPermissions;
--(NSString*)getEventByIdentifier:(NSString*)param;
--(BOOL)removeEvent:(NSString*)param;
+-(NSString*)openCalendar:(NSString*)param param1:(BOOL)param1;
+-(NSString*)saveEvent:(NSString*)param param1:(NSString*)param1 param2:(NSString*)param2 param3:(long long)param3 param4:(long long)param4 param5:(BOOL)param5 param6:(BOOL)param6 param7:(NSString*)param7 param8:(NSString*)param8 param9:(NSString*)param9;
+-(BOOL)removeEvent:(NSString*)param param1:(NSString*)param1;
+-(NSString*)getEventByID:(NSString*)param param1:(NSString*)param1;
+-(NSString*)getEvents:(NSString*)param param1:(long long)param1 param2:(long long)param2;
+-(void)registerForEventNotifications;
+-(void)deregisterForEventNotifications;
 -(BOOL)isSupported;
 @end
