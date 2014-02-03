78063801bc43ec26e4644a53deeb24f4efe978e3
lanerolle <lanerolle@gmail.com>
2014-02-03 20:36:22 +0000

    Auto generated native interface files

--- CN1Calendar/native/j2me/com/codename1/calendar/impl/CalendarNativeInterfaceImpl.java
+++ CN1Calendar/native/j2me/com/codename1/calendar/impl/CalendarNativeInterfaceImpl.java
@@ -1,30 +1,42 @@
 package com.codename1.calendar.impl;
 
 public class CalendarNativeInterfaceImpl {
-    public String getEvents(long param, long param1) {
+    public String getCalendarName(int param) {
         return null;
     }
 
-    public String saveEvent(String param, String param1, long param2, long param3, boolean param4, boolean param5, String param6, String param7, String param8) {
+    public String getEvents(String param, long param1, long param2) {
         return null;
-    }
-
-    public void deregisterForEventNotifications() {
-    }
-
-    public void registerForEventNotifications() {
     }
 
     public boolean hasPermissions() {
         return false;
     }
 
-    public String getEventByIdentifier(String param) {
+    public int getCalendarCount() {
+        return 0;
+    }
+
+    public String openCalendar(String param, boolean param1) {
         return null;
     }
 
-    public boolean removeEvent(String param) {
+    public String saveEvent(String param, String param1, String param2, long param3, long param4, boolean param5, boolean param6, String param7, String param8, String param9) {
+        return null;
+    }
+
+    public boolean removeEvent(String param, String param1) {
         return false;
+    }
+
+    public String getEventByID(String param, String param1) {
+        return null;
+    }
+
+    public void registerForEventNotifications() {
+    }
+
+    public void deregisterForEventNotifications() {
     }
 
     public boolean isSupported() {
